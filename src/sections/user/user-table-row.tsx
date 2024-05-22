3c7481b4bafcde2776c3e5e76200138fde382764
truonghoanghuy98 <truonghoanghuy98@gmail.com>
2024-05-22 14:40:15 +0700

    [front] - account & device

--- src/sections/user/user-table-row.tsx
+++ src/sections/user/user-table-row.tsx
@@ -1,15 +1,15 @@
 // @mui
 import IconButton from '@mui/material/IconButton';
 import ListItemText from '@mui/material/ListItemText';
-import MenuItem from '@mui/material/MenuItem';
 import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
+import Tooltip from '@mui/material/Tooltip';
 // hooks
 import { useBoolean } from 'src/hooks/use-boolean';
 // types
 import { IUserList } from 'src/types/user';
 // components
-import CustomPopover, { usePopover } from 'src/components/custom-popover';
+import { usePopover } from 'src/components/custom-popover';
 import Iconify from 'src/components/iconify';
 import Label from 'src/components/label';
 //
@@ -33,48 +33,39 @@ export default function UserTableRow({ row, selected, onEditRow, onSelectRow }: 
   const popover = usePopover();
 
   return (
-    <>
-      <TableRow hover selected={selected}>
-        <TableCell sx={{ display: 'flex', alignItems: 'center' }}>
-          <ListItemText primary={username} primaryTypographyProps={{ typography: 'body2' }} />
-        </TableCell>
+    <TableRow hover selected={selected}>
+      <TableCell sx={{ display: 'flex', alignItems: 'center' }}>
+        <ListItemText primary={username} primaryTypographyProps={{ typography: 'body2' }} />
+      </TableCell>
 
-        <TableCell>
-          <Label
-            variant="soft"
-            color={
-              (status === 'active' && 'success') || (status === 'banned' && 'error') || 'default'
-            }
+      <TableCell>
+        <Label
+          variant="soft"
+          color={
+            (status === 'active' && 'success') || (status === 'banned' && 'error') || 'default'
+          }
+        >
+          {status}
+        </Label>
+      </TableCell>
+
+      <TableCell align="center" sx={{ px: 1, whiteSpace: 'nowrap' }}>
+        <Tooltip title="Quick Edit" placement="top" arrow>
+          <IconButton
+            color={quickEdit.value ? 'inherit' : 'default'}
+            onClick={() => {
+              quickEdit.onTrue();
+              onEditRow();
+            }}
           >
-            {status}
-          </Label>
-        </TableCell>
-
-        <TableCell align="center" sx={{ px: 1, whiteSpace: 'nowrap' }}>
+            <Iconify icon="solar:pen-bold" />
+          </IconButton>
+        </Tooltip>
+        {/*
           <IconButton color={popover.open ? 'inherit' : 'default'} onClick={popover.onOpen}>
             <Iconify icon="eva:more-vertical-fill" />
-          </IconButton>
-        </TableCell>
-      </TableRow>
-
-      {/* <UserQuickEditForm currentUser={row} open={quickEdit.value} onClose={quickEdit.onFalse} /> */}
-
-      <CustomPopover
-        open={popover.open}
-        onClose={popover.onClose}
-        arrow="right-top"
-        sx={{ width: 140 }}
-      >
-        <MenuItem
-          onClick={() => {
-            onEditRow();
-            popover.onClose();
-          }}
-        >
-          <Iconify icon="solar:pen-bold" />
-          Edit
-        </MenuItem>
-      </CustomPopover>
-    </>
+          </IconButton> */}
+      </TableCell>
+    </TableRow>
   );
 }
