6795dd9d32c7f316163365003210e78092f4d7ac
truonghoanghuy98 <truonghoanghuy98@gmail.com>
2024-07-05 16:02:38 +0700

    [front] - update

--- src/sections/order/view/order-create-view.tsx
+++ src/sections/order/view/order-create-view.tsx
@@ -1,26 +1,39 @@
 'use client';
 
-import { useTheme } from '@mui/material/styles';
-import { Card, Container, MenuItem, Stack, Typography } from '@mui/material';
-import Grid from '@mui/system/Unstable_Grid/Grid';
-import React, { Fragment, useState } from 'react';
-import { RHFAutocomplete, RHFEditor, RHFSelect, RHFTextField } from 'src/components/hook-form';
-import { IOrderCreateOrUpdate } from 'src/types/order';
-import { Controller, DefaultValues, useFieldArray, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useSnackbar } from 'notistack';
-import * as Yup from 'yup';
-import { DatePicker } from '@mui/x-date-pickers';
-import { ICustomer } from 'src/types/customer';
-import { debounce } from 'lodash';
-import { getListCustomer } from 'src/api/customer';
-import FormProvider from 'src/components/hook-form/form-provider';
 import { LoadingButton } from '@mui/lab';
-import { useSettingsContext } from 'src/components/settings';
-import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
-import { paths } from 'src/routes/paths';
-import { IDetailDevice, IDevice } from 'src/types/product';
+import {
+  AutocompleteGetTagProps,
+  Box,
+  Button,
+  Card,
+  Chip,
+  Container,
+  InputAdornment,
+  Stack,
+  SxProps,
+  Theme,
+  Typography,
+} from '@mui/material';
+import Grid from '@mui/system/Unstable_Grid/Grid';
+import { DatePicker } from '@mui/x-date-pickers';
+import { debounce } from 'lodash';
+import { useSnackbar } from 'notistack';
+import React, { Fragment, useState } from 'react';
+import { Controller, DefaultValues, useFieldArray, useForm } from 'react-hook-form';
+import { getListCustomer } from 'src/api/customer';
 import { getListDevice } from 'src/api/product';
+import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
+import { RHFAutocomplete, RHFEditor, RHFTextField } from 'src/components/hook-form';
+import FormProvider from 'src/components/hook-form/form-provider';
+import Iconify from 'src/components/iconify';
+import { useSettingsContext } from 'src/components/settings';
+import { RouterLink } from 'src/routes/components';
+import { paths } from 'src/routes/paths';
+import { ICustomer } from 'src/types/customer';
+import { IOrderCreateOrUpdate } from 'src/types/order';
+import { IDevice } from 'src/types/product';
+import * as Yup from 'yup';
 
 const initializeDefaultValues = (): DefaultValues<IOrderCreateOrUpdate> => ({
   _id: undefined,
@@ -37,17 +50,34 @@ const initializeDefaultValues = (): DefaultValues<IOrderCreateOrUpdate> => ({
   note: '',
 });
 
+const styles: { [key: string]: SxProps<Theme> } = {
+  tag: {
+    cursor: 'pointer',
+    height: '26px',
+    borderRadius: '6px',
+
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    '& span': {
+      maxWidth: '220px',
+    },
+  },
+};
+
 export default function OrderCreateView() {
   const settings = useSettingsContext();
-  const theme = useTheme();
   const { enqueueSnackbar } = useSnackbar();
   const [customers, setCustomers] = useState<ICustomer[]>([]);
   const [deviceOptions, setDeviceOptions] = useState<{ [key: number]: IDevice[] }>({});
-  console.log('🚀 ~ OrderCreateView ~ deviceOptions:', deviceOptions);
+  const [price, setPrice] = useState<{ priceTotal: number; priceSale: number; priceTax: number }>({
+    priceTotal: 0,
+    priceSale: 0,
+    priceTax: 0,
+  });
 
   const NewOrderSchema = Yup.object().shape({
     delivery_date: Yup.string().required('Delivery date is required'),
-    totalAmount: Yup.number().required('Total amount is required'),
+    totalAmount: Yup.number().required().min(1, 'Total amount is required'),
     shipBy: Yup.string().required('ShipBy is required'),
     customer: Yup.string().required('Customer is required'),
     items: Yup.array()
@@ -55,7 +85,7 @@ export default function OrderCreateView() {
         Yup.object().shape({
           device: Yup.string().required('Device is required'),
           details: Yup.array()
-            .of(Yup.string().required('Detail is required'))
+            .min(1, 'At least one detail is required')
             .required('Details are required'),
         })
       )
@@ -81,7 +111,6 @@ export default function OrderCreateView() {
     name: 'items',
   });
 
-  console.log('🚀 ~ OrderCreateView ~ fields:', fields);
   const onSubmit = async (data: IOrderCreateOrUpdate) => {
     console.log('🚀 ~ onSubmit ~ data:', data);
   };
@@ -106,6 +135,12 @@ export default function OrderCreateView() {
       console.error('Failed to search devices:', error);
     }
   }, 300);
+
+  const renderTags = (value: string[], getTagProps: AutocompleteGetTagProps) => {
+    return value.map((option, index) => (
+      <Chip variant="outlined" label={option} {...getTagProps({ index })} sx={styles.tag} />
+    ));
+  };
 
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
@@ -182,11 +217,9 @@ export default function OrderCreateView() {
                     </Typography>
 
                     {fields?.map((item, index) => {
-                      console.log(deviceOptions?.[index]);
                       return (
                         <Fragment key={item?.id}>
                           <Grid container spacing={2}>
-                            {' '}
                             <Grid xs={4}>
                               <RHFAutocomplete
                                 key={item?.id}
@@ -210,15 +243,58 @@ export default function OrderCreateView() {
                                 }
                               />
                             </Grid>
-                            <Grid xs={4}>
+                            <Grid xs={6}>
                               <RHFAutocomplete
                                 key={item?.id}
                                 multiple
-                                name={`items?.${index}.details`}
+                                name={`items.${index}.details`}
                                 label="Chi tiết"
-                                options={deviceOptions?.[index]?.[0]?.detail || []}
+                                options={
+                                  (deviceOptions?.[index]?.[0]?.detail
+                                    ?.filter((x) => x.status === 'inventory')
+                                    ?.map((y) => y.id_device) as string[]) || []
+                                }
+                                noOptionsText="Vui lòng nhập cụm từ tìm kiếm của bạn."
+                                renderTags={renderTags}
                               />
                             </Grid>
+
+                            <Grid xs={1}>
+                              <Button
+                                variant="outlined"
+                                onClick={() => {
+                                  remove(index);
+                                  const clonedDeviceOptions = { ...deviceOptions };
+                                  delete clonedDeviceOptions[index];
+                                  const newData = Object.values(clonedDeviceOptions).reduce(
+                                    (acc: any, value, indexData) => {
+                                      acc[indexData] = value;
+                                      return acc;
+                                    },
+                                    {}
+                                  );
+                                  setDeviceOptions(newData);
+                                }}
+                                color="error"
+                                sx={{ height: '55px' }}
+                                disabled={fields?.length === 1}
+                              >
+                                <Iconify icon="eva:trash-2-outline" />
+                              </Button>
+                            </Grid>
+
+                            {index === 0 && (
+                              <Grid xs={1}>
+                                <Button
+                                  variant="outlined"
+                                  color="inherit"
+                                  onClick={() => append({ device: '', details: [] })}
+                                  sx={{ height: '55px' }}
+                                >
+                                  <Iconify icon="mingcute:add-line" />
+                                </Button>
+                              </Grid>
+                            )}
                           </Grid>
                         </Fragment>
                       );
@@ -236,14 +312,76 @@ export default function OrderCreateView() {
             </Card>
           </Grid>
 
+          <Grid xs={12} md={8}>
+            <Card>
+              <Stack spacing={3} sx={{ p: 3 }}>
+                <Typography variant="subtitle2">Định giá</Typography>
+                <Grid xs={12}>
+                  <RHFTextField name="totalAmount" label="Tổng tiền" type="number" />
+                </Grid>
+                <Grid xs={12}>
+                  <RHFTextField
+                    name=""
+                    label="Giảm giá"
+                    placeholder="Nhập giá giảm"
+                    type="number"
+                    value={price.priceSale || ''}
+                    onChange={(e) => {
+                      setPrice({ ...price, priceSale: Number(e.target.value) });
+                    }}
+                  />
+                </Grid>
+                <Grid xs={12}>
+                  <RHFTextField
+                    name=""
+                    label="Thuế (%)"
+                    placeholder="0.00"
+                    type="number"
+                    InputLabelProps={{ shrink: true }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="start">
+                          <Box component="span" sx={{ color: 'text.disabled' }}>
+                            %
+                          </Box>
+                        </InputAdornment>
+                      ),
+                    }}
+                    value={price?.priceTax || ''}
+                    onChange={(e) => {
+                      const value = Number(e.target.value);
+                      setPrice({ ...price, priceTax: value });
+                    }}
+                  />
+                </Grid>
+
+                <Grid
+                  xs={12}
+                  sx={{ display: 'flex', alignItems: 'center', justifyContent: 'right' }}
+                >
+                  <Typography>Tổng: {watch('totalAmount')}</Typography>
+                </Grid>
+              </Stack>
+            </Card>
+          </Grid>
+
           <Grid
             xs={12}
             md={8}
-            sx={{ display: 'flex', alignItems: 'center', justifyContent: 'right' }}
+            sx={{ display: 'flex', alignItems: 'center', justifyContent: 'right', gap: '5px' }}
           >
-            <LoadingButton type="submit" variant="contained" size="large" loading={isSubmitting}>
+            <LoadingButton
+              type="submit"
+              variant="contained"
+              size="large"
+              loading={isSubmitting}
+              sx={{ height: '36px' }}
+            >
               {'Tạo mới'}
             </LoadingButton>
+            <Button component={RouterLink} href={paths.dashboard.order.new} variant="contained">
+              Hủy
+            </Button>
           </Grid>
         </Grid>
       </FormProvider>
