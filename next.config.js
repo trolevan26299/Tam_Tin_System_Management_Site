a2f91c023c1c6bf34837228debbd84e7443e872b
andrew <trolevan26299@gmail.com>
2025-02-26 00:51:39 +0700

    update code

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ module.exports = {
       transform: '@mui/lab/{{member}}',
     },
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://167.172.64.193:6969/api/:path*', // Thay thế bằng URL backend của bạn
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
